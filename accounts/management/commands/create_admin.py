f8096f16f2972eb485ab231a8de93951defdc02c
HamadMulti <informhammadahmad@gmail.com>
2025-07-02 14:49:56 +0300

    hotfix admin

--- accounts/management/commands/create_admin.py
+++ accounts/management/commands/create_admin.py
@@ -9,15 +9,13 @@ class Command(BaseCommand):
         User = get_user_model()
 
         admin_email = os.getenv("ADMIN_EMAIL", "taskhorizonapp@gmail.com")
-        admin_username = os.getenv("ADMIN_USERNAME", "hamad")
         admin_password = os.getenv("ADMIN_PASSWORD", "hamad@123@")
 
         if not User.objects.filter(is_superuser=True).exists():
             User.objects.create_superuser(
-                username=admin_username,
                 email=admin_email,
                 password=admin_password
             )
-            self.stdout.write(self.style.SUCCESS(f"Superuser '{admin_username}' created successfully."))
+            self.stdout.write(self.style.SUCCESS(f"Superuser '{admin_email}' created successfully."))
         else:
             self.stdout.write(self.style.WARNING("Superuser already exists. No action taken."))
